7c2c7640ed2883a2fc496d0a99db7536ba6ee59d
Mauro I. Dominguez <mau_igna_06@hotmail.com>
2023-07-04 16:49:53 -0300

    BUG: delete resampled curve after titanium plate design

--- BoneReconstructionPlanner/BoneReconstructionPlanner.py
+++ BoneReconstructionPlanner/BoneReconstructionPlanner.py
@@ -4204,6 +4204,8 @@ Then click 'Create 3D model of reconstruction...' again.""")
     extrusionModel.CreateDefaultDisplayNodes()
     extrusionModel.SetAndObservePolyData(triangleFilter.GetOutput())
 
+    slicer.mrmlScene.RemoveNode(plateCurveResampled)
+
   def pointsOverlap(self,firstPolygonPoints,secondPolygonPoints):
     firstPolygonPoints_np = np.array(firstPolygonPoints, dtype=np.float)
     secondPolygonPoints_np = np.array(secondPolygonPoints, dtype=np.float)
