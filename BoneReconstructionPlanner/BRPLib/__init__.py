fcd94da3eb2f0c91404dd5b54ecb6438bb13954f
Mauro I. Dominguez <mau_igna_06@hotmail.com>
2025-11-10 17:54:56 -0300

    ENH: improve folder handling logic

---
+++ BoneReconstructionPlanner/BRPLib/__init__.py
@@ -0,0 +1,5 @@
+# BRPLib - Bone Reconstruction Planner Library
+# This package contains helper functions and GUI widgets for the Bone Reconstruction Planner module
+
+from .helperFunctions import *
+from .guiWidgets import *
